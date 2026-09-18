c5fa5dadb45d9c86d048c2191fd4aca0c9461402
l86385138-pixel <l86385138@gmail.com>
2026-09-18 08:22:49 +0530

    Configure TECHEAR ADMIN release APK: coaching-apk/app/src/main/java/technology/greatindia/coaching/MainActivity.java

--- coaching-apk/app/src/main/java/technology/greatindia/coaching/MainActivity.java
+++ coaching-apk/app/src/main/java/technology/greatindia/coaching/MainActivity.java
@@ -1,3 +1,38 @@
 package technology.greatindia.techearadmin;
-import android.app.*; import android.os.*; import android.graphics.Color; import android.webkit.*;
-public class MainActivity extends Activity { public void onCreate(Bundle b){super.onCreate(b); WebView w=new WebView(this); w.setBackgroundColor(Color.WHITE); w.getSettings().setJavaScriptEnabled(true); w.getSettings().setDomStorageEnabled(true); w.getSettings().setMediaPlaybackRequiresUserGesture(false); w.setWebViewClient(new WebViewClient()); w.setWebChromeClient(new WebChromeClient()); w.loadUrl("https://coaching.greatindia.technology/"); setContentView(w);} }
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String START_URL =
+        "https://coaching.greatindia.technology/?utm_source=chatgpt.com/c/kartar-classes";
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        WebView w = new WebView(this);
+        w.setBackgroundColor(Color.WHITE);
+        WebSettings s = w.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setAllowFileAccess(false);
+        s.setAllowContentAccess(true);
+        w.setWebViewClient(new WebViewClient());
+        w.setWebChromeClient(new WebChromeClient());
+        w.loadUrl(START_URL);
+        setContentView(w);
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView w = (WebView) findViewById(android.R.id.content);
+        if (w != null && w.canGoBack()) w.goBack();
+        else super.onBackPressed();
+    }
+}
